9b1297bd46238e06b01a104c8cc2d37f7c9b5f05
Amadeo Abud <amaabud123@gmail.com>
2023-08-01 10:43:48 -0300

    Añadido nodemon

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express();
+app.get("/", (req, res) =>{
+   res.sendFile("./Views/index.html")
+})
  app.listen(3000, ()=>{
     console.log("BIENVENIDOS!")
  })
