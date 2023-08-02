0b889055094ec008b3a04628f07b3d518a8acc76
JaGz2107 <proyectagz@gmail.com>
2023-08-02 00:39:02 -0300

    home actualizado

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require("express")
+const express = require('express');
+const path = require('path');
 const app = express();
-const path = require("path")
+
+app.use(express.static(path.resolve(__dirname, './public')))
+
+app.listen(3000, () => console.log('APP corriendo en el puerto:' + 3000))
+
 app.get("/", (req, res) =>{
-   let routePath = path.resolve(__dirname, "./Views/index.html")
-   res.sendFile(routePath);
+   res.sendfile(path.resolve(__dirname, "./views/home.html"))
 })
- app.listen(3000, ()=>{
-    console.log("BIENVENIDOS!")
- })
